a39a625da4164cd8bd301e76bae6c4d1d3898d1b
UnionOne <hax2033@gmail.com>
2016-05-07 16:51:19 +0300

    heroku: simple restfull post service
    Former-commit-id: 788de141a63448a6c28f290bde7577bebeff453b

---
+++ src/main/java/com/github/union/blog/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package com.github.union.blog.repository;
+
+import com.github.union.blog.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * Created by union on 7/05/16.
+ */
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    @Query(value = "SELECT p FROM Post p WHERE p.id = ?1")
+    Post findOnePostById(Integer id);
+}
