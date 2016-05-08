370226294069bbb7a1676994f5a43e06101a6987
Vladilslav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-05-08 18:30:18 +0300

    put controller service implemented

--- src/main/java/com/github/union/blog/repository/PostRepository.java
+++ src/main/java/com/github/union/blog/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.github.union.blog.repository;
 
 import com.github.union.blog.domain.Post;
+import com.github.union.blog.dto.PostModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +15,9 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 
     @Query(value = "SELECT p FROM Post p WHERE p.id = ?1")
     Post findOnePostById(Integer id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Post p SET p.content = ?1 WHERE p.id = ?2")
+    void updateContentById(String content,Integer id);
 }
