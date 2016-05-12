e9ef45364859f74593f32dbd6361a417f0b42f45
UnionOne <hax2033@gmail.com>
2016-05-12 16:15:03 +0300

    master: added global CORS configuration
    Former-commit-id: e97600d301f4304b1b7e0dc273f35f0e85aa9449

---
+++ src/main/java/com/github/union/blog/WebConfig.java
@@ -0,0 +1,20 @@
+package com.github.union.blog;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by union on 12.05.16.
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**");
+    }
+}
