98af49d1aea88040b1e9d18e0c68d53eb413a273
UnionOne <hax2033@gmail.com>
2016-05-13 13:55:27 +0300

    master: rename import.sql to data/return to embed in memory h2
    Former-commit-id: dbbcf454788bcca9ef663126d7a559a66f4f8fc0

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
+VALUES (1, 'title', 'subtitle', 'content', 'date', 'author');
+
+INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
+VALUES (2, 'title', 'subtitle', 'content', 'date', 'author');
+
+INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
+VALUES (3, 'title', 'subtitle', 'content', 'date', 'author');
+
+INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
+VALUES (4, 'title', 'subtitle', 'content', 'date', 'author');
+
+INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
+VALUES (5, 'title', 'subtitle', 'content', 'date', 'author');
+
+INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
+VALUES (6, 'title', 'subtitle', 'content', 'date', 'author');
