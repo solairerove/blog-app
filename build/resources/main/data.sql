4d3bf2e58844c6878ab83a0a5952f671c88add7c
UnionOne <hax2033@gmail.com>
2016-05-15 17:19:48 +0300

    master: using ngResource instead of
    Former-commit-id: 95063d7ad6c2aa46bfa4af4c972fd9f5c4dfb53e

--- build/resources/main/data.sql
+++ build/resources/main/data.sql
@@ -17,7 +17,7 @@ VALUES (
   2,
   'I believe every human has a finite number of heartbeats. I don''t intend to waste any of mine.',
   'Problems look mighty small.',
-  '<p><em>NASA</em> is not about the ‘Adventure of Human Space Exploration’…We won’t be doing it just to get out there in space – we’ll be doing it because the things we learn out there will be making life better for a lot of people who won’t be able to go.</p>
+  '<p>NASA is not about the ‘Adventure of Human Space Exploration’…We won’t be doing it just to get out there in space – we’ll be doing it because the things we learn out there will be making life better for a lot of people who won’t be able to go.</p>
   <p>The dreams of yesterday are the hopes of today and the reality of tomorrow.</p>
   <p>As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked so fragile, so delicate, that if you touched it with a finger it would crumble and fall apart. Seeing this has to change a man.</p>',
   '2016-05-11',
