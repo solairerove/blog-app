a82201c2bc7f796a1b82ad08b828356a373691fd
UnionOne <hax2033@gmail.com>
2016-05-15 11:58:32 +0300

    master: update post's content only

--- build/resources/main/data.sql
+++ build/resources/main/data.sql
@@ -1,17 +1,61 @@
 INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
-VALUES (1, 'title', 'subtitle', 'content', 'date', 'author');
+VALUES (
+  1,
+  'Man must explore, and this is exploration at its greatest.',
+  'Problems look mighty small from 150 miles up.',
+  '<p>What was most significant about the lunar voyage was not that man set foot on the Moon but that they set eye on the earth.</p>
+  <p>Houston, Tranquillity Base here. The Eagle has landed.</p>
+  <p>NASA is not about the ‘Adventure of Human Space Exploration’…We won’t be doing it just to get out there in space – we’ll be doing it because the things we learn out there will be making life better for a lot of people who won’t be able to go.</p>
+  <p>The Earth was small, light blue, and so touchingly alone, our home that must be defended like a holy relic. The Earth was absolutely round. I believe I never knew what the word round meant until I saw Earth from space.</p>
+  <p>When I orbited the Earth in a spaceship, I saw for the first time how beautiful our planet is. Mankind, let us preserve and increase this beauty, and not destroy it!</p>',
+  '2016-05-11',
+  'union.one'
+);
 
 INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
-VALUES (2, 'title', 'subtitle', 'content', 'date', 'author');
+VALUES (
+  2,
+  'I believe every human has a finite number of heartbeats. I don''t intend to waste any of mine.',
+  'Problems look mighty small.',
+  '<p><em>NASA</em> is not about the ‘Adventure of Human Space Exploration’…We won’t be doing it just to get out there in space – we’ll be doing it because the things we learn out there will be making life better for a lot of people who won’t be able to go.</p>
+  <p>The dreams of yesterday are the hopes of today and the reality of tomorrow.</p>
+  <p>As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked so fragile, so delicate, that if you touched it with a finger it would crumble and fall apart. Seeing this has to change a man.</p>',
+  '2016-05-11',
+  'vlad'
+);
 
 INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
-VALUES (3, 'title', 'subtitle', 'content', 'date', 'author');
+VALUES (
+  3,
+  'Science has not yet mastered prophecy.',
+  'We predict too much for the next year and yet far too little for the next ten.',
+  '<p>We want to explore. We’re curious people. Look back over history, people have put their lives at stake to go out and explore … We believe in what we’re doing. Now it’s time to go.</p>
+  <p>We choose to go to the moon in this decade and do the other things, not because they are easy, but because they are hard, because that goal will serve to organize and measure the best of our energies and skills, because that challenge is one that we are willing to accept, one we are unwilling to postpone, and one which we intend to win.</p>
+  <p>Buy why, some say, the moon? Why choose this as our goal? And they may as well ask why climb the highest mountain?</p>',
+  '2016-05-11',
+  'union.one'
+);
 
 INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
-VALUES (4, 'title', 'subtitle', 'content', 'date', 'author');
+VALUES (
+  4,
+  'Failure is not an option.',
+  'Many say exploration is part of our destiny, but it’s actually our duty to future generations.',
+  '<p>There can be no thought of finishing for ‘aiming for the stars.’ Both figuratively and literally, it is a task to occupy the generations. And no matter how much progress one makes, there is always the thrill of just beginning.</p>
+  <p>There can be no thought of finishing for ‘aiming for the stars.’ Both figuratively and literally, it is a task to occupy the generations. And no matter how much progress one makes, there is always the thrill of just beginning.</p>
+  <p>Science has not yet mastered prophecy. We predict too much for the next year and yet far too little for the next 10.</p>',
+  '2016-05-11',
+  'union.one'
+);
 
 INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
-VALUES (5, 'title', 'subtitle', 'content', 'date', 'author');
-
-INSERT INTO `POST` (`id`, `title`, `subtitle`, `content`, `date`, `author`)
-VALUES (6, 'title', 'subtitle', 'content', 'date', 'author');
+VALUES (
+  5,
+  'MockFlow new feature: Link your comments in the Mockup.',
+  'This is the part 2 follow up feature to our SiteMap implementation.',
+  '<p>We choose to go to the moon in this decade and do the other things, not because they are easy, but because they are hard, because that goal will serve to organize and measure the best of our energies and skills, because that challenge is one that we are willing to accept, one we are unwilling to postpone, and one which we intend to win.</p>
+  <p>For those who have seen the Earth from space, and for the hundreds and perhaps thousands more who will, the experience most certainly changes your perspective. The things that we share in our world are far more valuable than those which divide us.</p>
+  <p>Across the sea of space, the stars are other suns.</p>',
+  '2016-05-11',
+  'vlad'
+);
