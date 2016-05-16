4e3ca44d670af2817462ba0d9d20aaedac74ede1
Vladilslav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-05-16 10:43:53 +0300

    random utils added, commons lang dependencies added

--- src/test/com/github/union/blog/repository/PostRepositoryTest.java
+++ src/test/com/github/union/blog/repository/PostRepositoryTest.java
@@ -1,7 +1,39 @@
 package com.github.union.blog.repository;
 
+import com.github.union.blog.domain.Post;
+import com.github.union.blog.service.PostService;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * Created by vlad on 16.05.16.
  */
 public class PostRepositoryTest {
+    private final static Logger LOGGER = Logger.getLogger(PostRepository.class);
+
+    @Autowired
+    PostRepository postRepository;
+    @Autowired
+    PostService postService;
+
+    @Before
+    public void setUp(){
+        LOGGER.info("********* SETTING UP SUCCESSFUL *********");
+        postService.save(new Post("test title","test subtitle","test content","test date","test author"));
+    }
+
+    @After
+    public void down(){
+        LOGGER.info("********* SETTING DOWN SUCCESSFUL *********");
+    }
+
+    @Test
+    public void findOneByPostIdTest(){
+        LOGGER.info("********* FINDING BY POST ID TEST *********");
+
+    }
+
 }
