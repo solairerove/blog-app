224a9346a98925e15ff966533a2203f7d4fdbf12
UnionOne <hax2033@gmail.com>
2016-05-16 13:57:56 +0300

    master: added separate controllers
    Former-commit-id: 565251c21179b36b5d2ed5a8b14decc6eb71d125

--- src/app/controllers.js
+++ src/app/controllers.js
@@ -3,18 +3,3 @@
  */
 
 var blog = angular.module('blog', ['ngRoute', 'ngSanitize', 'ngResource']);
-
-/* Controllers */
-blog.controller('PostListCtrl', ['$scope', 'MainFactory',
-    function ($scope, MainFactory) {
-        $scope.posts = MainFactory.query();
-    }]);
-
-blog.controller('PostCtrl', ['$scope', '$location', '$routeParams', 'PostFactory',
-    function ($scope, $location, $routeParams, PostFactory) {
-
-        PostFactory.get({id: $routeParams.id}, function (data) {
-            $scope.post = data;
-            $scope.content = $scope.post.content;
-        });
-    }]);
