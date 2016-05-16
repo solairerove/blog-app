63a6d59a1ab41fc9f57a18f3dcf2c9089064dceb
Vladilslav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-05-16 12:16:07 +0300

    repository JUnit tests added

--- src/main/java/com/github/union/blog/repository/PostRepository.java
+++ src/main/java/com/github/union/blog/repository/PostRepository.java
@@ -4,6 +4,7 @@ import com.github.union.blog.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -16,7 +17,7 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     @Query(value = "SELECT p FROM Post p WHERE p.id = ?1")
     Post findOnePostById(Integer id);
 
-    @Modifying
+    @Modifying(clearAutomatically = true)
     @Query(value = "UPDATE Post p SET p.content = ?1 WHERE p.id = ?2")
     void updateContentById(String content, Integer id);
 }
