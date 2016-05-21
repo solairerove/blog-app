7845a365a2cc39ebda1c20aa49a13552fe7ef452
UnionOne <hax2033@gmail.com>
2016-05-21 15:17:28 +0300

    master: simple rest client on angular2

---
+++ client/app/post.ts
@@ -0,0 +1,9 @@
+export class Post {
+    constructor(public title:string,
+                public subtitle:string,
+                public content:string,
+                public date:string,
+                public author:string) {
+        
+    }
+}
