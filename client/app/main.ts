125f1e951d48a5350ee533a54448dd9c06045131
UnionOne <hax2033@gmail.com>
2016-05-22 10:52:52 +0300

    master: added posts component stub
    Former-commit-id: 87b5dc7438bbab5283f97129dbd2997379226ab3

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,5 +1,4 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
-import {HTTP_PROVIDERS} from 'angular2/http';
 
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
+bootstrap(AppComponent);
---
+++ server/src/main/java/com/github/union/blog/service/PostService.java
@@ -0,0 +1,27 @@
+package com.github.union.blog.service;
+
+import com.github.union.blog.domain.Post;
+import com.github.union.blog.dto.PostDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by union on 7/05/16.
+ */
+
+@Component
+public interface PostService {
+
+    List<Post> findAll();
+
+    Post findOnePostById(Integer id);
+
+    void save(Post post);
+
+    void deletePostById(Integer id);
+
+    void updateContentById(PostDTO postDTO);
+
+    void deleteAllPosts();
+}
