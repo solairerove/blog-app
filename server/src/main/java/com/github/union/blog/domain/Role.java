73de7a0ac3d7f6b966c65c6bb0a326f2f37c9e9c
Vladilslav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-05-22 16:58:56 +0300

    user model prototype implemented
    Former-commit-id: d4d11e519093433c06a0c1941e28be9e373e2473

--- server/src/main/java/com/github/union/blog/domain/Role.java
+++ server/src/main/java/com/github/union/blog/domain/Role.java
@@ -18,6 +18,22 @@ public class Role {
         this.name = name;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
