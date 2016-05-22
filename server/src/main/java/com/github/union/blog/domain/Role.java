4811879509f33f9b40c22aba9a79ebce218c3233
Vladilslav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-05-22 16:49:56 +0300

    role model prototype implemented

---
+++ server/src/main/java/com/github/union/blog/domain/Role.java
@@ -0,0 +1,47 @@
+package com.github.union.blog.domain;
+
+/**
+ * Created by vlad on 22.05.16.
+ */
+public class Role {
+
+    private Integer id;
+
+    private String name;
+
+    public Role() {
+
+    }
+
+    public Role(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (id != null ? !id.equals(role.id) : role.id != null) return false;
+        return name != null ? name.equals(role.name) : role.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ server/src/main/java/com/github/union/blog/domain/User.java
@@ -0,0 +1,15 @@
+package com.github.union.blog.domain;
+
+import java.util.List;
+
+/**
+ * Created by vlad on 22.05.16.
+ */
+public class User {
+
+    private String login;
+    private String password;
+    private List<Post> posts;
+
+
+}
