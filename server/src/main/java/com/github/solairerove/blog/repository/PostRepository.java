04e55673263e6b446bb4f7de3f8f3ad6c0e1e009
solairerove <hax2033@gmail.com>
2016-05-25 10:32:52 +0300

    master: rename package

---
+++ server/src/main/java/com/github/solairerove/blog/repository/PostRepository.java
@@ -0,0 +1,22 @@
+package com.github.solairerove.blog.repository;
+
+import com.github.solairerove.blog.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by union on 7/05/16.
+ */
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    @Query(value = "SELECT p FROM Post p WHERE p.id = ?1")
+    Post findOnePostById(Integer id);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE Post p SET p.content = ?1 WHERE p.id = ?2")
+    void updateContentById(String content, Integer id);
+}
