6c9cf49afcd43ffba3cdd2e4d03f2bb975900a31
solairerove <hax2033@gmail.com>
2016-05-25 10:32:52 +0300

    master: rename package
    Former-commit-id: 04e55673263e6b446bb4f7de3f8f3ad6c0e1e009

---
+++ server/src/test/java/com/github/solairerove/blog/repository/PostRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.github.solairerove.blog.repository;
+
+import com.github.solairerove.blog.Application;
+import com.github.solairerove.blog.domain.Post;
+import com.github.solairerove.blog.service.PostService;
+import com.github.solairerove.blog.repository.common.EntityUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by vlad on 16.05.16.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@WebAppConfiguration
+@SpringApplicationConfiguration(Application.class)
+public class PostRepositoryTest {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private PostService postService;
+
+    @Before
+    public void setUp() {
+        postService.save(EntityUtils.generatePost());
+    }
+
+    @Test
+    public void findOneByPostIdTest() {
+        Post saved = EntityUtils.generatePost();
+        postService.save(saved);
+
+        Post found = postRepository.findOnePostById(saved.getId());
+
+        Assert.assertEquals(saved, found);
+    }
+
+    @Test
+    public void updateContentByIdTest() {
+        Post saved = EntityUtils.generatePost();
+        postService.save(saved);
+
+        postRepository.updateContentById("test content", saved.getId());
+
+        Post found = postRepository.findOnePostById(saved.getId());
+        Assert.assertEquals("test content", found.getContent());
+    }
+}
---
+++ server/src/test/java/com/github/solairerove/blog/repository/common/EntityUtils.java
@@ -0,0 +1,28 @@
+package com.github.solairerove.blog.repository.common;
+
+import com.github.solairerove.blog.domain.Post;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by vlad on 16.05.16.
+ */
+
+@Component
+@Transactional
+public class EntityUtils {
+    private static final int MAX_STRING_LENGTH = 25;
+
+    private static String getRandomString(int length) {
+        return RandomStringUtils.random(length, true, true);
+    }
+
+    public static Post generatePost() {
+        return new Post(getRandomString(MAX_STRING_LENGTH),
+                getRandomString(MAX_STRING_LENGTH),
+                getRandomString(MAX_STRING_LENGTH),
+                getRandomString(MAX_STRING_LENGTH),
+                getRandomString(MAX_STRING_LENGTH));
+    }
+}
