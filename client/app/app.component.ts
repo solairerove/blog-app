8ea28e70b7f663bde2b3807e375f6c579e4e4704
Krivitski Nikita <nikita.krivitski@azati.com>
2016-05-25 09:29:40 +0300

    master: implemented simple navigation bar

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,13 +1,38 @@
 import {Component} from 'angular2/core';
-import {PostService} from './post.service';
+import {
+    RouteConfig,
+    ROUTER_DIRECTIVES,
+    ROUTER_PROVIDERS
+} from 'angular2/router';
 
+import {PostService} from './post.service';
 import {PostsComponent} from './posts.component';
+import {AboutComponent} from './about.component';
+
+@RouteConfig([
+    {
+        path: '/posts',
+        name: 'Posts',
+        component: PostsComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: AboutComponent
+    }
+])
 
 @Component({
     selector: 'my-app',
-    template: '<my-posts></my-posts>',
-    directives: [PostsComponent],
-    providers: [PostService]
+    templateUrl: 'app/app.component.html',
+    directives: [
+        ROUTER_DIRECTIVES
+    ],
+    providers: [
+        ROUTER_PROVIDERS,
+        PostService
+    ]
 })
 
 export class AppComponent {
