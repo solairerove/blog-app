0cdfd3b78c21e3a8ae0b75b95656c2ec42ffaa53
solairerove <hax2033@gmail.com>
2016-05-25 14:35:18 +0300

    master: implemented custom footer component

---
+++ client/app/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'footer',
+    templateUrl: '/app/components/footer/footer.component.html'
+})
+
+export class FooterComponent {
+
+}
+
