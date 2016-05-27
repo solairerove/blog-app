0347498f60e6b9183404e05cc708a3bd6a605488
vlsidlyarevich <vladislav.sidlyarevich@azati.com>
2016-05-27 15:09:32 +0300

    master:comment service on client added

---
+++ client/app/service/comment.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from "angular2/core";
+import {Http, Response} from "angular2/http";
+import {Observable} from "rxjs/Observable";
+import "rxjs/Rx";
+import {Comment} from "../model/comment";
+
+@Injectable()
+export class CommentService{
+    
+    private url:string;
+    
+    constructor(private http:Http){
+        this.url='http://localhost:8080/api/posts/'
+    }
+    
+    getPostComments(postId):Observable<Comment[]>{
+        return this.http.get(this.url + postId + '/' 
+            + 'comments')
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
+    
+    private handleError(error:Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }    
+}
