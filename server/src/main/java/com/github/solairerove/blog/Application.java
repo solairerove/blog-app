d7ee53d316f8df2664ed7cdf5f06991281c823ca
UnionOne <hax2033@gmail.com>
2016-05-28 15:47:26 +0300

    master: added component scan annotation to Applivation class
    Former-commit-id: adb788f97b89fb5cb18835b2455ddb17fc130ad2

--- server/src/main/java/com/github/solairerove/blog/Application.java
+++ server/src/main/java/com/github/solairerove/blog/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by union on 04.05.16.
  */
 @SpringBootApplication
+@ComponentScan("com.github.solairerove.blog")
 public class Application {
 
     public static void main(String[] args) {
