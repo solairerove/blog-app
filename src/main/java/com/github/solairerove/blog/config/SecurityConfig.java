05d5b22bc993edcc3c1679f8ad9cc680e127795a
solairerove <hax2033@gmail.com>
2016-05-31 13:17:26 +0300

    master: delete console log
    Former-commit-id: 930b0164d1bf59781c8e7ebd4f8f941f882e04e1

--- src/main/java/com/github/solairerove/blog/config/SecurityConfig.java
+++ src/main/java/com/github/solairerove/blog/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.github.solairerove.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,8 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public final static String AUTHORIZATION_HEADER = "x-auth-token";
-
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
@@ -29,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/**").authenticated();
     }
 
+    @Bean
     public AuthenticationEntryPoint authenticationEntryPoint() {
         return (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied");
     }
