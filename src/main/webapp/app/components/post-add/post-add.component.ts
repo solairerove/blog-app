8c01749f8d8aea34e771306c6f6ba49ecd0295b1
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-06-03 16:49:48 +0300

    #6 - add post component created/app component route config to new component added

---
+++ src/main/webapp/app/components/post-add/post-add.component.ts
@@ -0,0 +1,8 @@
+import {Component, OnInit} from 'angular2/core';
+
+@Component({
+    selector: 'my-post-add-component',
+    templateUrl: '/app/components/post-add/post-add.component.html',
+})
+
+export class PostAddComponent {}
