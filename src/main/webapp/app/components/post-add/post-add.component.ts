d080f83a221e96e985fcdcb32b89b4e6f4b82d27
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-06-03 16:55:54 +0300

    #6 - post add component implementation
    Former-commit-id: 04335d58df4c61919546f1eaaf994797876ce5ba

--- src/main/webapp/app/components/post-add/post-add.component.ts
+++ src/main/webapp/app/components/post-add/post-add.component.ts
@@ -1,8 +1,22 @@
 import {Component, OnInit} from 'angular2/core';
+import {HTTP_PROVIDERS} from 'angular2/http';
+
+import {PostService} from "../../service/post.service";
 
 @Component({
     selector: 'my-post-add-component',
     templateUrl: '/app/components/post-add/post-add.component.html',
+    providers: [
+        PostService,
+        HTTP_PROVIDERS
+    ]
 })
 
-export class PostAddComponent {}
+export class PostAddComponent {
+
+    constructor(private postService:PostService) {
+
+    }
+    
+    
+}
