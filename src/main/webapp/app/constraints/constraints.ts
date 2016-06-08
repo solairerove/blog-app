c4b6a272205cd64b17cf71a3e576ed8f2093bc0f
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-06-08 16:29:47 +0300

    #19 - urls added

--- src/main/webapp/app/constraints/constraints.ts
+++ src/main/webapp/app/constraints/constraints.ts
@@ -1,7 +1,9 @@
-export class Constraints{
+export class Constraints {
     public static POSTS_PATH = '/posts';
     public static ABOUT_PATH = '/about';
     public static POST_DETAIL_PATH = '/posts/:id';
     public static ADD_POST_PATH = '/add';
     public static OTHER = '/**';
+    public static POST_URL = 'http://localhost:8080/api/posts';
+    public static POST_URL_WITH_END_SLASH = 'http://localhost:8080/api/posts/';
 }
--- src/main/webapp/app/service/comment.service.ts
+++ src/main/webapp/app/service/comment.service.ts
@@ -2,26 +2,28 @@ import {Injectable} from "angular2/core";
 import {Http, Response} from "angular2/http";
 import {Observable} from "rxjs/Observable";
 import "rxjs/Rx";
+
+import {Constraints} from '../constraints/constraints';
 import {Comment} from "../model/comment";
 
 @Injectable()
-export class CommentService{
-    
+export class CommentService {
+
     private url:string;
-    
-    constructor(private http:Http){
-        this.url='http://localhost:8080/api/posts/'
+
+    constructor(private http:Http) {
+        this.url = Constraints.POST_URL_WITH_END_SLASH;
     }
-    
-    getPostComments(id):Observable<Comment[]>{
+
+    getPostComments(id):Observable<Comment[]> {
         return this.http.get(this.url + id + '/'
-            + 'comments')
+                + 'comments')
             .map(res => res.json())
             .catch(this.handleError);
     }
-    
+
     private handleError(error:Response) {
         console.error(error);
         return Observable.throw(error.json().error || 'Server error');
-    }    
+    }
 }
