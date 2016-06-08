e42d5b776ae2402ab38eba9be43ab50b43400897
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-06-08 16:19:49 +0300

    #19 - paths constants added
    Former-commit-id: 37eed04bce8fb5d85d4b947668ee3d19cda69285

---
+++ src/main/webapp/app/constraints/constraints.ts
@@ -0,0 +1,7 @@
+export class Constraints{
+    public static POSTS_PATH = '/posts';
+    public static ABOUT_PATH = '/about';
+    public static POST_DETAIL_PATH = '/posts/:id';
+    public static ADD_POST_PATH = '/add';
+    public static OTHER = '/**';
+}
