6119987b03548cfb5937019e57de97bbf8122093
solairerove <hax2033@gmail.com>
2016-06-17 13:17:41 +0300

    liferay: SURPRISE MOTHERFU***R

--- src/main/java/com/github/solairerove/blog/Application.java
+++ src/main/java/com/github/solairerove/blog/Application.java
@@ -2,6 +2,8 @@ package com.github.solairerove.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -9,7 +11,12 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan("com.github.solairerove.blog")
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
