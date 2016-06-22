6442ec6d32f128f1c2ce4dcd927f05409a7903e5
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-06-22 11:02:52 +0300

    liferay: paths added

--- src/main/webapp/app/constraints/constraints.ts
+++ src/main/webapp/app/constraints/constraints.ts
@@ -1,9 +1,12 @@
 export class Constraints {
-    public static POSTS_PATH = '/posts';
-    public static ABOUT_PATH = '/about';
-    public static POST_DETAIL_PATH = '/posts/:id';
-    public static ADD_POST_PATH = '/add';
-    public static OTHER = '/**';
-    public static POST_URL = 'http://localhost:8080/api/posts';
-    public static POST_URL_WITH_END_SLASH = 'http://localhost:8080/api/posts/';
+
+    private static BASIC_PATH = 'liferay-blog-backend-0.0.1-SNAPSHOT';
+
+    public static POSTS_PATH = this.BASIC_PATH + '/posts';
+    public static ABOUT_PATH = this.BASIC_PATH + '/about';
+    public static POST_DETAIL_PATH = this.BASIC_PATH + '/posts/:id';
+    public static ADD_POST_PATH = this.BASIC_PATH + '/add';
+    public static OTHER = this.BASIC_PATH + '/**';
+    public static POST_URL = 'http://localhost:8080' + this.BASIC_PATH + '/api/posts';
+    public static POST_URL_WITH_END_SLASH = 'http://localhost:8080' + this.BASIC_PATH + '/api/posts/';
 }
