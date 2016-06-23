0aad8956260e0e6389d960ba3b42e8996e88b3f4
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-06-23 12:13:04 +0300

    #12 - view styling

--- src/main/webapp/app/components/log-in/log-in.component.ts
+++ src/main/webapp/app/components/log-in/log-in.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, Input} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+
 import {User} from "../../model/user";
 import {UserService} from "../../service/user.service";
 
@@ -14,14 +15,15 @@ import {UserService} from "../../service/user.service";
     directives: [ROUTER_DIRECTIVES]
 })
 
-
 export class LogInComponent implements OnInit {
 
     @Input() user:User;
     error:any;
+    authenticated:boolean;
     token:string;
 
     constructor(private userService:UserService) {
+        this.authenticated = false;
     }
 
     ngOnInit() {
@@ -31,6 +33,7 @@ export class LogInComponent implements OnInit {
     authenticate() {
         this.userService.authenticate(this.user)
             .subscribe(data => this.token = JSON.stringify(data));
+        this.authenticated = true;
         console.log(this.token);
     }
 }
