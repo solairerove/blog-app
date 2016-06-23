36f7e2d08172493ba0f7478a93d60e013affa25f
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-06-23 10:39:25 +0300

    #21 - tests complete

--- src/test/java/com/github/solairerove/blog/controller/AuthControllerTest.java
+++ src/test/java/com/github/solairerove/blog/controller/AuthControllerTest.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.solairerove.blog.Application;
 import com.github.solairerove.blog.domain.User;
 import com.github.solairerove.blog.dto.LoginDTO;
+import com.github.solairerove.blog.security.provider.SecurityProvider;
 import com.github.solairerove.blog.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 /**
@@ -38,6 +42,9 @@ public class AuthControllerTest {
     @Autowired
     private UserService service;
 
+    @Autowired
+    private SecurityProvider provider;
+
     private MockMvc mvc;
 
     @Before
@@ -50,8 +57,8 @@ public class AuthControllerTest {
     }
 
     @Test
-    public void authenticateTest() throws Exception{
-        LoginDTO dto = new LoginDTO("admin","pass");
+    public void authenticateTest() throws Exception {
+        LoginDTO dto = new LoginDTO("admin", "pass");
         ObjectMapper objectMapper = new ObjectMapper();
 
         mvc.perform(MockMvcRequestBuilders.request(HttpMethod.POST, "/api/authenticate")
@@ -59,6 +66,61 @@ public class AuthControllerTest {
                 .content(objectMapper.writeValueAsString(dto))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.token",notNullValue()));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.token", notNullValue()));
+    }
+
+    @Test
+    public void authenticateUnknownUserTest() throws Exception {
+        LoginDTO dto = new LoginDTO("unknown", "pass");
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        mvc.perform(MockMvcRequestBuilders.request(HttpMethod.POST, "/api/authenticate")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    public void currentUserTest() throws Exception {
+        String token = Jwts.builder().setSubject("admin").
+                signWith(SignatureAlgorithm.HS512, provider.getTokenKey()).compact();
+
+        mvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Rest-Token", token)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.login",is("admin")));
+    }
+
+    @Test
+    public void currentUnauthorizedUserTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Rest-Token", "wrong_token")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    public void logoutTest() throws Exception {
+        String token = Jwts.builder().setSubject("admin").
+                signWith(SignatureAlgorithm.HS512, provider.getTokenKey()).compact();
+
+        mvc.perform(MockMvcRequestBuilders.request(HttpMethod.POST, "/api/logout")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Rest-Token", token)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void logoutUnauthorizedTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.request(HttpMethod.POST, "/api/logout")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Rest-Token", "wrong_token")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 }
